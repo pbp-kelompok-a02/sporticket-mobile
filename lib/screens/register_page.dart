0d37b044e3e96b77eb6a6ccad4c6e357bd82b21b
Fidan <fidankhalilsi24@gmail.com>
2025-12-21 22:02:52 +0700

    fix register page

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -368,13 +368,6 @@ class _RegisterPageState extends State<RegisterPage> {
                                                           Colors.green,
                                                     ),
                                                   );
-                                                  // Navigator.pushReplacement(
-                                                  //   context,
-                                                  //   MaterialPageRoute(
-                                                  //     builder: (context) =>
-                                                  //         const LoginPage(),
-                                                  //   ),
-                                                  // );
                                                   Navigator.pop(context, true);
                                                 } else {
                                                   ScaffoldMessenger.of(
